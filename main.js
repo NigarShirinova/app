9fc51968e6ba5b1b91905c80cc580f7fdfed8c6c
NigarShirinova <nigaresh@code.edu.az>
2024-10-26 22:53:42 +0400

    ready

--- main.js
+++ main.js
@@ -1,15 +1,31 @@
 const audioPlayer = document.getElementById('audio-player');
 const dots = document.querySelectorAll('.dot');
+const keyAudioMap = {};
+let currentKey = null;  
 
 dots.forEach(dot => {
-    dot.addEventListener('mousedown', function() {
-        const audioSrc = dot.getAttribute('data-audio');
-        audioPlayer.src = audioSrc;
-        audioPlayer.play();
-    });
+    const audioSrc = dot.getAttribute('data-audio');
+    const char = dot.getAttribute('data-char').toUpperCase();
+    keyAudioMap[char] = audioSrc;
+});
 
-    dot.addEventListener('mouseup', function() {
+
+document.addEventListener('keydown', function(event) {
+    const key = event.key.toUpperCase();
+    
+    if (keyAudioMap[key] && currentKey !== key) {  /
+        audioPlayer.src = keyAudioMap[key];
+        audioPlayer.play();
+        currentKey = key;  
+    }
+});
+
+document.addEventListener('keyup', function(event) {
+    const key = event.key.toUpperCase();
+    
+    if (key === currentKey) {  
         audioPlayer.pause();
-        audioPlayer.currentTime = 0; 
-    });
+        audioPlayer.currentTime = 0;  
+        currentKey = null;  
+    }
 });
